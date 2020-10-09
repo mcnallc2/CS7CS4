5e243ff88e4a1057ffc276cdf9d9950fa41cdae0
mcnallc2 <mcnallc2@tcd.ie>
2020-10-09 12:09:02 +0100

    week2 adding square features

--- week2/week2.py
+++ week2/week2.py
@@ -13,6 +13,83 @@ X2 = df.iloc[:,1]
 X = np.column_stack((X1,X2))
 y = df.iloc[:,2]
 
+# ## (a) (i)
+# plt.figure(1)
+# for i, val in enumerate(y):
+#     if val == 1:
+#         plt.scatter(X[i][0], X[i][1], s=60, color='blue', marker='+')
+#     elif val == -1:
+#         plt.scatter(X[i][0], X[i][1], s=30, color='black', marker='o')
+
+# ## (a) (ii)
+# model = LogisticRegression(penalty='none',solver='lbfgs').fit(X,y)
+# print(f'penalty - none')
+# print(f'intercept - {model.intercept_[0]}')
+# print(f'slope_1 - {model.coef_[0][0]}')
+# print(f'slope_2 - {model.coef_[0][1]}\n')
+
+
+# pred = model.predict(X)
+# for i, val in enumerate(pred):
+#     if val == 1:
+#         plt.scatter(X[i][0], X[i][1], s=25, color='green', marker='+')
+#     elif val == -1:
+#         plt.scatter(X[i][0], X[i][1], s=5, color='yellow', marker='o')
+
+# ## h(x) = w0 + w1x1 + w2x2
+# ## h(x) = 0 - @ desision boundary
+# ## 0 = w0 + w1x1 + w2x2
+# ## equation for x2: x2 = -(w0 + w1x1)/w2
+# x1_bound = np.linspace(-1, 1, 100)
+# x2_bound = -(model.intercept_[0] + (x1_bound * model.coef_[0][0])) / model.coef_[0][1]
+# plt.plot(x1_bound, x2_bound, '--', c="red")
+
+# plt.figure(1)
+# plt.title('Logistic Regression predictions and desision boundary')
+# plt.xlabel('X1 features')
+# plt.ylabel('X2 features')
+# plt.legend(['desision boundary'])
+
+# for i, penalty in enumerate([0.001, 1, 1000]):
+
+#     plt.figure(i+2)
+#     for i, val in enumerate(y):
+#         if val == 1:
+#             plt.scatter(X[i][0], X[i][1], s=60, color='blue', marker='+')
+#         elif val == -1:
+#             plt.scatter(X[i][0], X[i][1], s=30, color='black', marker='o')
+
+#     model = LinearSVC(C=penalty, max_iter=90000).fit(X,y)
+#     print(f'penalty - {penalty}')
+#     print(f'intercept - {model.intercept_[0]}')
+#     print(f'slope_1 - {model.coef_[0][0]}')
+#     print(f'slope_2 - {model.coef_[0][1]}\n')
+
+#     pred = model.predict(X)
+#     for i, val in enumerate(pred):
+#         if val == 1:
+#             plt.scatter(X[i][0], X[i][1], s=25, color='green', marker='+')
+#         elif val == -1:
+#             plt.scatter(X[i][0], X[i][1], s=5, color='yellow', marker='o')
+
+#     ## h(x) = w0 + w1x1 + w2x2
+#     ## h(x) = 0 - @ desision boundary
+#     ## 0 = w0 + w1x1 + w2x2
+#     ## equation for x2: x2 = -(w0 + w1x1)/w2
+#     x1_bound = np.linspace(-1, 1, 100)
+#     x2_bound = -(model.intercept_[0] + (x1_bound * model.coef_[0][0])) / model.coef_[0][1]
+#     plt.plot(x1_bound, x2_bound, '--', c="red")
+
+#     plt.title(f'Linear SVM (C={float(penalty)}) and desision boundary')
+#     plt.xlabel('X1 features')
+#     plt.ylabel('X2 features')
+#     plt.legend(['desision boundary'])
+
+
+X1_2 = X1**2
+X2_2 = X2**2
+X_2 = np.column_stack((X1,X2,X1_2,X2_2))
+
 ## (a) (i)
 plt.figure(1)
 for i, val in enumerate(y):
@@ -22,60 +99,32 @@ for i, val in enumerate(y):
         plt.scatter(X[i][0], X[i][1], s=30, color='black', marker='o')
 
 ## (a) (ii)
-model = LogisticRegression(penalty='none',solver='lbfgs').fit(X,y)
-print(f'intercept {model.intercept_[0]}, slope_1 {model.coef_[0][0]}, slope_2 {model.coef_[0][1]}')
+model = LogisticRegression(penalty='none',solver='lbfgs').fit(X_2,y)
+print(f'penalty - none')
+print(f'intercept - {model.intercept_}')
+#print(f'slope_1 - {model.coef_[0][0]}')
+print(f'slope_2 - {model.coef_}\n')
 
-pred = model.predict(X)
+pred = model.predict(X_2)
 for i, val in enumerate(pred):
     if val == 1:
         plt.scatter(X[i][0], X[i][1], s=25, color='green', marker='+')
     elif val == -1:
         plt.scatter(X[i][0], X[i][1], s=5, color='yellow', marker='o')
 
-## h(x) = w0 + w1x1 + w2x2
-## h(x) = 0 - @ desision boundary
-## 0 = w0 + w1x1 + w2x2
-## equation for x2: x2 = -(w0 + w1x1)/w2
+t0 = model.intercept_[0]
+t1 = model.coef_[0][0]
+t2 = model.coef_[0][1]
+t3 = model.coef_[0][2]
+t4 = model.coef_[0][3]
+
 x1_bound = np.linspace(-1, 1, 100)
-x2_bound = -(model.intercept_[0] + (x1_bound * model.coef_[0][0])) / model.coef_[0][1]
+x2_bound = (-(t2) + np.sqrt((t2**2) - (4 * t4 * (t0 + (t1 * x1_bound) + (t3 * (x1_bound**2)))))) / (2 * t4)
 plt.plot(x1_bound, x2_bound, '--', c="red")
 
 
-plt.figure(2)
-for i, val in enumerate(y):
-    if val == 1:
-        plt.scatter(X[i][0], X[i][1], s=60, color='blue', marker='+')
-    elif val == -1:
-        plt.scatter(X[i][0], X[i][1], s=30, color='black', marker='o')
 
-
-for penalty in [0.001, 1, 1000]:
-
-    model = LinearSVC(C=penalty, max_iter=90000).fit(X,y)
-    print(f'penalty - {penalty}')
-    print(f'intercept - {model.intercept_[0]}')
-    print(f'slope_1 - {model.coef_[0][0]}')
-    print(f'slope_2 - {model.coef_[0][1]}\n')
-
-    pred = model.predict(X)
-    for i, val in enumerate(pred):
-        if val == 1:
-            plt.scatter(X[i][0], X[i][1], s=25, color='green', marker='+')
-        elif val == -1:
-            plt.scatter(X[i][0], X[i][1], s=5, color='yellow', marker='o')
-
-    ## h(x) = w0 + w1x1 + w2x2
-    ## h(x) = 0 - @ desision boundary
-    ## 0 = w0 + w1x1 + w2x2
-    ## equation for x2: x2 = -(w0 + w1x1)/w2
-    x1_bound = np.linspace(-1, 1, 100)
-    x2_bound = -(model.intercept_[0] + (x1_bound * model.coef_[0][0])) / model.coef_[0][1]
-    plt.plot(x1_bound, x2_bound, '--', c="red")
-
-
-
-plt.figure(1)
-# plt.title('')
+plt.title(f'Logistic Regression 4 features and desision boundary')
 plt.xlabel('X1 features')
 plt.ylabel('X2 features')
 plt.legend(['desision boundary'])
